603b2a4b9ef7fe9713c11a30b2adb860cde78ff9
snayeemu <snayeemu2@gmail.com>
2025-02-01 15:14:05 +0600

    navigation bar added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ProfileNavbar from "@/components/shared/ProfileNavbar";
+import ProfileNavbar from "@/components/shared/ProfileNavbar/ProfileNavbar";
+import Navbar from "@/components/shared/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ProfileNavbar />
+        <Navbar />
         {children}
       </body>
     </html>
